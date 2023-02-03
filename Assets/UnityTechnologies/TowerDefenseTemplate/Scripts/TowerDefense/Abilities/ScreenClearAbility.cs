32a1b9c5957c6947b4d17f807e59f19cb5ca43e9
Michael Law <michael.law@ontariotechu.net>
2023-02-03 03:00:06 -0500

    Power ups v1
    - powerups are referred to as abilities for now
    - system of creating and adding new abilities has been developed
    - basic abilities UI has been made functional

---
+++ Assets/UnityTechnologies/TowerDefenseTemplate/Scripts/TowerDefense/Abilities/ScreenClearAbility.cs
@@ -0,0 +1,35 @@
+using Core.Health;
+using TowerDefense.Agents;
+using TowerDefense.Level;
+using UnityEngine;
+
+namespace TowerDefense.Abilities
+{
+    public class ScreenClearAbility : Ability
+    {
+        /// <summary>
+        /// The alignment of the damager
+        /// </summary>
+        public SerializableIAlignmentProvider alignment;
+
+        public IAlignmentProvider alignmentProvider
+        {
+            get { return alignment != null ? alignment.GetInterface() : null; }
+        }
+
+        public override void Update()
+        {
+            Debug.Log("a");
+            if (currentDuration > 0)
+            {
+                for (int i = 0; i < LevelManager.instance.activeAgents.Count; i++)
+                {
+                    Agent temp = LevelManager.instance.activeAgents[i];
+                    temp.TakeDamage(temp.configuration.currentHealth, temp.position, alignmentProvider);
+                
+                }
+            }
+            base.Update();
+        }
+    }
+}
